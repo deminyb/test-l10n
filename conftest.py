3cb16511770cab6ba5af5f3998835e4cd2ec48dd
Iurii Demin <YDemin@it-one.ru>
2023-06-05 17:21:46 +0300

    Localization test

---
+++ conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='ru', help='Choose language')
+
+@pytest.fixture(scope='function')
+def browser(request):
+    language = request.config.getoption("language")
+    options = webdriver.ChromeOptions()
+    options.add_argument(f"lang={language}")
+    print(f"\nStarting browser with language = {language}")
+    browser = webdriver.Chrome(chrome_options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium.webdriver.common.by import By
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_items(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert button, f"Add to basket button isn't found"
