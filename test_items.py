a205abfd21022c4546b996a1de4be22c82a7a6e1
Iurii Demin <YDemin@it-one.ru>
2023-06-05 17:42:05 +0300

    Fix test name

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_items(browser):
+def test_add_to_basket_button_exists(browser):
     browser.get(link)
     time.sleep(30)
     button = browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
